97c62d71f013941a3d448f446bc31deaa3561c2d
yu-gotcha <djWjfkrh98>
2021-04-18 15:00:46 +0900

    implemented file upload operation

--- src/main/java/kr/ac/sejong/api/controller/FileUploadController.java
+++ src/main/java/kr/ac/sejong/api/controller/FileUploadController.java
@@ -1,20 +1,101 @@
 package kr.ac.sejong.api.controller;
 
 
+import kr.ac.sejong.api.domain.UploadImg;
+import kr.ac.sejong.api.domain.UploadVid;
+import kr.ac.sejong.api.domain.User;
+import kr.ac.sejong.api.repository.UploadImgRepository;
+import kr.ac.sejong.api.repository.UploadVidRepository;
+import kr.ac.sejong.api.service.FileUploadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpSession;
+import java.io.File;
 
 @Controller
 @RequestMapping(value = "")
 public class FileUploadController {
+    private final FileUploadService fileUploadService;
+    private final UploadImgRepository uploadImgRepository;
+    private final UploadVidRepository uploadVidRepository;
+
+    @Autowired
+    FileUploadController(FileUploadService fileUploadService, UploadImgRepository uploadImgRepository, UploadVidRepository uploadVidRepository) {
+        this.fileUploadService = fileUploadService;
+        this.uploadImgRepository = uploadImgRepository;
+        this.uploadVidRepository = uploadVidRepository;
+    }
+
     @GetMapping( value = "upload")
     public String upload() { return "upload"; }
 
+
     @PostMapping( value = "/upload" )
-    public String uploadProcess(String user, String content){
+    @ResponseStatus(HttpStatus.CREATED)
+    /*
+    public String upload(@RequestPart MultipartFile file, HttpSession session) throws Exception{
+        String savedFileName;
+        User user;
+        long count;
+
+        user=(User)session.getAttribute("userdata");
+        count=uploadImgRepository.findByImgUpUser(user).size();
+
+        String originalFileName = file.getOriginalFilename();
+        savedFileName=Long.toString(user.getUserId())+"_"+Long.toString(count)+"_"+originalFileName;
+        File dest = new File("C:/Image/"+savedFileName);
+        file.transferTo(dest);
 
 
+        fileUploadService.saveImg(file.getOriginalFilename(), savedFileName, dest.toString(), user);
+
+        return "redirect:/upload";
     }
+    */
+    public String upload(@RequestPart MultipartFile imgFile, MultipartFile vidFile, HttpSession session) throws Exception{
+        String savedFileName;
+        User user;
+        long count;
+
+        user=(User)session.getAttribute("userdata");
+
+        //Image Upload
+        count=uploadImgRepository.findByImgUpUser(user).size();
+
+        String originalFileName = imgFile.getOriginalFilename();
+        savedFileName=Long.toString(user.getUserId())+"_"+Long.toString(count)+"_"+originalFileName;
+        File dest = new File("C:/Image/"+savedFileName);
+        imgFile.transferTo(dest);
+
+        fileUploadService.saveImg(imgFile.getOriginalFilename(), savedFileName, dest.toString(), user);
+
+        //Video Upload
+        count=uploadVidRepository.findByVidUpUser(user).size();
+
+        originalFileName = vidFile.getOriginalFilename();
+        savedFileName=Long.toString(user.getUserId())+"_"+Long.toString(count)+"_"+originalFileName;
+        dest = new File("C:/Image/"+savedFileName);
+        vidFile.transferTo(dest);
+
+        fileUploadService.saveVid(vidFile.getOriginalFilename(), savedFileName, dest.toString(), user);
+
+
+        try{
+            UploadImg upImg = uploadImgRepository.findTopByOrderByUpImgIdDesc();
+            UploadVid upVid = uploadVidRepository.findTopByOrderByUpVidIdDesc();
+
+            fileUploadService.saveUpload(upImg, upVid, user);
+        }catch (Exception e){
+            System.out.println(e);
+            return "redirect:/";
+        }
+
+        return "redirect:/upload";
+    }
+
+
 }
