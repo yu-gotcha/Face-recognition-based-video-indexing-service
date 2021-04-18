06a93bac8b46d9083e69931a30e2aab2d5eb3291
yu-gotcha <djWjfkrh98>
2021-04-18 15:02:46 +0900

    implemented service about file data related to upload operation

---
+++ src/main/java/kr/ac/sejong/api/service/FileUploadService.java
@@ -0,0 +1,78 @@
+package kr.ac.sejong.api.service;
+
+import kr.ac.sejong.api.domain.Upload;
+import kr.ac.sejong.api.domain.UploadImg;
+import kr.ac.sejong.api.domain.UploadVid;
+import kr.ac.sejong.api.domain.User;
+import kr.ac.sejong.api.repository.UploadImgRepository;
+import kr.ac.sejong.api.repository.UploadRepository;
+import kr.ac.sejong.api.repository.UploadVidRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileUploadService {
+    private final UploadImgRepository UpImgRepository;
+    private final UploadVidRepository UpVidRepository;
+    private final UploadRepository uploadRepository;
+
+    @Autowired
+    public FileUploadService(UploadImgRepository UpImgRepository,UploadVidRepository UpVidRepository, UploadRepository uploadRepository) {
+        this.UpImgRepository = UpImgRepository;
+        this.UpVidRepository = UpVidRepository;
+        this.uploadRepository = uploadRepository;
+    }
+
+    public Boolean saveImg(String fileName, String fileSavedName, String filePath, User user){
+        UploadImg img = new UploadImg();
+
+        img.setUpImgName(fileName);
+        img.setUpImgSavedName(fileSavedName);
+        img.setUpImgPath(filePath);
+        img.setImgUpUser(user);
+
+        try {
+            UpImgRepository.save(img);
+        }catch (Exception e){
+            System.out.println(e);
+            return false;
+        }
+
+        return true;
+    }
+
+    public Boolean saveVid(String fileName, String fileSavedName, String filePath, User user){
+        UploadVid vid = new UploadVid();
+
+        vid.setUpVidName(fileName);
+        vid.setUpVidSavedName(fileSavedName);
+        vid.setUpVidPath(filePath);
+        vid.setVidUpUser(user);
+
+        try {
+            UpVidRepository.save(vid);
+        }catch (Exception e){
+            System.out.println(e);
+            return false;
+        }
+
+        return true;
+    }
+
+    public Boolean saveUpload(UploadImg upImg, UploadVid upVid, User user){
+        Upload upload = new Upload();
+
+        upload.setUpImg(upImg);
+        upload.setUpVid(upVid);
+        upload.setUser(user);
+
+        try{
+            uploadRepository.save(upload);
+        }catch(Exception e){
+            System.out.println(e);
+            return false;
+        }
+
+        return true;
+    }
+}
