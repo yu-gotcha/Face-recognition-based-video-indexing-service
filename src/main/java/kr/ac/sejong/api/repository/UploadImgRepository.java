97d92c82a0cec93ef53b27431a96fe32a7f05e0d
yu-gotcha <yu.gotcha@gmail.com>
2021-05-11 17:08:44 +0900

    added annotation

--- src/main/java/kr/ac/sejong/api/repository/UploadImgRepository.java
+++ src/main/java/kr/ac/sejong/api/repository/UploadImgRepository.java
@@ -3,9 +3,11 @@ package kr.ac.sejong.api.repository;
 import kr.ac.sejong.api.domain.UploadImg;
 import kr.ac.sejong.api.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UploadImgRepository extends JpaRepository <UploadImg, Long> {
     List<UploadImg> findAll();
     List<UploadImg> findByImgUpUser(User user);
--- src/main/java/kr/ac/sejong/api/repository/UserRepository.java
+++ src/main/java/kr/ac/sejong/api/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByLoginId(String loginId);
     Boolean existsByLoginId(String loginId);
