ffff309eb1f7a1c1117a377412b4357674921372
yu-gotcha <yu.gotcha@gmail.com>
2021-05-11 16:48:59 +0900

    revised repository repository

--- src/main/java/kr/ac/sejong/api/service/FileService.java
+++ src/main/java/kr/ac/sejong/api/service/FileService.java
@@ -2,9 +2,7 @@ package kr.ac.sejong.api.service;
 
 import kr.ac.sejong.api.domain.Upload;
 import kr.ac.sejong.api.domain.User;
-import kr.ac.sejong.api.repository.UploadImgRepository;
 import kr.ac.sejong.api.repository.UploadRepository;
-import kr.ac.sejong.api.repository.UploadVidRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -16,13 +14,9 @@ import java.util.Map;
 @Service
 public class FileService {
     private final UploadRepository uploadRepository;
-    private final UploadImgRepository uploadImgRepository;
-    private final UploadVidRepository uploadVidRepository;
 
-    public FileService(UploadRepository uploadRepository, UploadImgRepository uploadImgRepository, UploadVidRepository uploadVidRepository) {
+    public FileService(UploadRepository uploadRepository) {
         this.uploadRepository = uploadRepository;
-        this.uploadImgRepository = uploadImgRepository;
-        this.uploadVidRepository = uploadVidRepository;
     }
 
 
@@ -41,8 +35,8 @@ public class FileService {
 
             map.put("no", i.getUpId());
             //이름 가져올 수 있어야함
-            map.put("imageFileName", uploadImgRepository.findByUpImgId(i.getUpId()).getUpImgName());
-            map.put("videoFileName", uploadVidRepository.findByUpVidId(i.getUpId()).getUpVidName());
+            map.put("imageFileName", i.getUploadImg().getUpImgName());
+            map.put("videoFileName", i.getUploadVid().getUpVidName());
 
             if(i.getUploading()==1) uploading = "upload completed";
             else uploading = "uploading...";
@@ -52,8 +46,8 @@ public class FileService {
             else processing = "processing...";
             map.put("process", processing);
 
-            map.put("imgSavedName", uploadImgRepository.findByUpImgId(i.getUpId()).getUpImgSavedName());
-            map.put("vidSavedName", uploadVidRepository.findByUpVidId(i.getUpId()).getUpVidSavedName());
+            map.put("imgSavedName", i.getUploadImg().getUpImgSavedName());
+            map.put("vidSavedName", i.getUploadVid().getUpVidSavedName());
 
             fileList.add(map);
         }
