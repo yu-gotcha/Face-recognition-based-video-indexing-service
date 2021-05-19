5606494a6ede4e3422fc38b323fd5fbc8c456f65
yu-gotcha <yu.gotcha@gmail.com>
2021-05-20 03:07:14 +0900

    added async configuration

---
+++ src/main/java/kr/ac/sejong/api/Config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package kr.ac.sejong.api.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig extends AsyncConfigurerSupport {
+
+    @Override
+    public Executor getAsyncExecutor(){
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("py-async-");
+        executor.initialize();
+        return executor;
+    }
+}
