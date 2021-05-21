dd0dc30916d5821001508c9c6e87cd7f64d5f5f8
yu-gotcha <yu.gotcha@gmail.com>
2021-05-22 01:42:24 +0900

    added find method

--- src/main/java/kr/ac/sejong/api/repository/ResultSectionRepository.java
+++ src/main/java/kr/ac/sejong/api/repository/ResultSectionRepository.java
@@ -1,9 +1,15 @@
 package kr.ac.sejong.api.repository;
 
 import kr.ac.sejong.api.domain.ResultSection;
+import kr.ac.sejong.api.domain.Upload;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ResultSectionRepository extends JpaRepository<ResultSection, Long> {
+    List<ResultSection> findAllByUpload(long uploadId);
+    List<ResultSection> findByUploadOOrderByStartAsc(long uploadId);
+    Upload findByUpload(long uploadId);
 }
